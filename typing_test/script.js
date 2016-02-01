d92ca6766b56abca210402de602f076310172b1e
Drew-Killeen <drewkilleen00@gmail.com>
2016-02-01 14:26:11 -0500

    Script updates

--- typing_test/script.js
+++ typing_test/script.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
-    $("#type").keypress(function() {
-        console.log( "Handler for .keypress() called." );
-    });
-});
+var d;
 
 function words() {
     var words = ["time", "person", "year", "way", "day", "thing", "man", "world", "life", "hand", "part", "child", "eye", "woman", "place", "work", "week", "case", "point", "government", "company", "number", "group", "problem", "fact"],
-        output = "", 
+        output = "",
         i;
-    for(i = 0; i <= 200; i++) {
-        if(i % 10 === 0) {
+    for (i = 0; i <= 200; i++) {
+        if (i % 10 === 0) {
             output += "<br>"
         }
         output += words[Math.floor((Math.random() * 25) + 1)] + " ";
@@ -18,16 +14,19 @@ function words() {
 }
 
 function startCountdown() {
-    d = new Date();
-    countdown = d.getMilliseconds();
+    if (d === undefined) {
+        d = new Date();
+        countdown = d.getMilliseconds();
+        console.log(countdown);
+    }
 }
 
 function type() {
-    if(d.getMilliseconds() === countdown) {
+    if (d.getMilliseconds() === countdown) {
         endCountdown();
     }
 }
 
 function endCountdown() {
-    
+
 }
