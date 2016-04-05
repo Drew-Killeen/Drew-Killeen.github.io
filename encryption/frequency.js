e02b380461c3f40864107142f79ce536b7649d05
Drew-Killeen <drewkilleen00@gmail.com>
2016-04-05 16:31:38 -0500

    Add default button

--- encryption/frequency.js
+++ encryption/frequency.js
@@ -1,4 +1,6 @@
 document.addEventListener("keypress", analyze);
+document.getElementById("default").addEventListener("mousedown", defaultFrequency);
+document.addEventListener("mouseup", analyze);
 
 function analyze() {
     var input, letter;
@@ -15,6 +17,14 @@ function analyze() {
         } else {
             window.myBar.datasets[0].bars[i].value = input.match(letter || {}).length;
         }
+    }
+    window.myBar.update();
+}
+
+function defaultFrequency() {
+    var defaultValues = [8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153, 0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056, 2.758, 0.978, 2.361, 0.150, 1.974, 0.074];
+    for(var i = 0; i < 26; i++) {
+        window.myBar.datasets[0].bars[i].value = defaultValues[i];
     }
     window.myBar.update();
 }
