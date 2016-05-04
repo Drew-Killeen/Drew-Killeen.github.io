258680a36cc9242f72e6d19b83bf556d586a7042
Drew-Killeen <drewkilleen00@gmail.com>
2016-05-04 14:27:15 -0400

    Revert "Revert "grammar""
    This reverts commit fa17c2a34910dd07fd7584e61b880880d1b8848a.

--- typing_test/script.js
+++ typing_test/script.js
@@ -1,5 +1,17 @@
-var d;
-document.getElementById("type").addEventListener("keypress", startCountdown);
+var d, j = 0,
+    l = 0,
+    start = false,
+    wordlist = new Array,
+    correctwords = 0,
+    wrongwords = 0,
+    txt = "",
+    testlength = 60;
+document.getElementById("type").addEventListener("keypress", updateClock);
+document.getElementById("type").addEventListener("keypress", function (e) {
+    if (32 == e.keyCode) {
+        checkWord();
+    }
+});
 
 function words() {
     var words = ["time", "person", "year", "way", "day", "thing", "man", "world", "life", "hand", "part", "child", "eye", "woman", "place", "work", "week", "case", "point", "government", "company", "number", "group", "problem", "fact"],
@@ -9,28 +21,45 @@ function words() {
         if (i % 10 === 0) {
             output += "<br>"
         }
-        output += words[Math.floor((Math.random() * 25) + 1)] + " ";
-        document.getElementById("type_me").innerHTML = output;
+        wordlist[i] = words[Math.floor((Math.random() * 25))];
+        output += '<span id="word' + i + '">' + wordlist[i] + "</span> ";
+    }
+    document.getElementById("type_me").innerHTML = output;
+    document.getElementById("word0").classList.add("highlight");
+}
+
+function updateClock() {
+    testlength = Number(document.getElementById("testLength").value);
+    if (start === false) {
+        document.getElementById("type").removeEventListener("keypress", updateClock);
+    }
+    start = true;
+    var time = testlength - j;
+    j++;
+    document.getElementById("clock").innerHTML = time + " seconds";
+    if (j < testlength + 1) {
+        setTimeout(updateClock, 1000);
+    } else {
+        endTest();
     }
 }
 
-/*
- ** Countdown functions - needs lots of work 
- */
-function startCountdown() {
-    if (d === undefined) {
-        d = new Date();
-        countdown = d.getMilliseconds();
-        console.log(countdown);
-    }
+function checkWord() {
+    txt += document.getElementById("type").value;
+    document.getElementById("type").value = "";
+    document.getElementById("word" + l).classList.remove("highlight");
+    l++;
+    document.getElementById("word" + l).classList.add("highlight");
 }
 
-function type() {
-    if (d.getMilliseconds() === countdown) {
-        endCountdown();
+function endTest() {
+    txt = txt.split(" ");
+    for (var i = 0; i < txt.length; i++) {
+        if (txt[i] == wordlist[i]) {
+            correctwords++;
+        } else {
+            wrongwords++;
+        }
     }
-}
-
-function endCountdown() {
-
+    alert("Test completed... " + correctwords * (60 / testlength) + " WPM");
 }
