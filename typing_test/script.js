1b2a0dd7a535113517dfbc75ebf2033b683640da
Drew-Killeen <drewkilleen00@gmail.com>
2016-05-05 14:18:25 -0400

    cosmetic changes

--- typing_test/script.js
+++ typing_test/script.js
@@ -5,7 +5,8 @@ var d, j = 0,
     correctwords = 0,
     wrongwords = 0,
     txt = "",
-    testlength = 60;
+    testlength = 60,
+    output;
 document.getElementById("type").addEventListener("keypress", updateClock);
 document.getElementById("type").addEventListener("keypress", function (e) {
     if (32 == e.keyCode) {
@@ -14,32 +15,45 @@ document.getElementById("type").addEventListener("keypress", function (e) {
 });
 
 function words() {
-    var words = ["time", "person", "year", "way", "day", "thing", "man", "world", "life", "hand", "part", "child", "eye", "woman", "place", "work", "week", "case", "point", "government", "company", "number", "group", "problem", "fact"],
-        output = "",
-        i;
-    for (i = 0; i <= testlength * 3; i++) {
+    var words = ["time", "person", "year", "way", "day", "thing", "man", "world", "life", "hand", "part", "child", "eye", "woman", "place", "work", "week", "case", "point", "government", "company", "number", "group", "problem", "fact"];
+    output = "<span id='row0'>";
+    for (var i = 0; i <= testlength * 3; i++) {
         if (i % 10 === 0 && i !== 0) {
-            output += "<br>"
+            output += "</span><br><span class='doNotDisplay' id='row" + i / 10 + "'>"
         }
         wordlist[i] = words[Math.floor((Math.random() * 25))];
         output += '<span id="word' + i + '">' + wordlist[i] + "</span> ";
     }
+    output += '</span>'
     document.getElementById("type_me").innerHTML = output;
     document.getElementById("word0").classList.add("highlight");
+    document.getElementById("row1").classList.remove("doNotDisplay");
+    document.getElementById("row2").classList.remove("doNotDisplay");
     var style = document.createElement('style');
     style.type = 'text/css';
     for (var i = 0; i < (wordlist.length * 3) / 10; i++) {
-        style.innerHTML += '.row' + i + ' {position:relative;top:-' + i * 49 + 'px;} ';
+        style.innerHTML += '.currentrow' + i + ' {position:relative;top:-' + i * 49 + 'px;} ';
         document.getElementsByTagName('head')[0].appendChild(style);
+    }
+}
+
+/* 
+ ** Add words function - still in progress. Will finish when it becomes a priority.
+ */
+function addWords() {
+    for (var i = 0; i <= testlength * 3; i++) {
+        if (i % 10 === 0 && i !== 0) {
+            output += "</span><br><span class='doNotDisplay' id='row" + i / 10 + "'>"
+        }
+        wordlist[i] = words[Math.floor((Math.random() * 25))];
+        output += '<span id="word' + i + '">' + wordlist[i] + "</span> ";
     }
 }
 
 function updateClock() {
     testlength = Number(document.getElementById("testLength").value);
-    if (testlength < 1) {
+    if (testlength < 1 || testlength === NaN) {
         testlength = 60;
-    } else if (testlength > 60 && start === false) {
-        words();
     }
     if (start === false) {
         document.getElementById("type").removeEventListener("keypress", updateClock);
@@ -62,8 +76,10 @@ function checkWord() {
     l++;
     document.getElementById("word" + l).classList.add("highlight");
     if (l % 10 === 0 && l !== 0) {
-        document.getElementById("type_me").classList.remove("row" + (l / 10 - 1));
-        document.getElementById("type_me").classList.add("row" + l / 10);
+        document.getElementById("type_me").classList.remove("currentrow" + (l / 10 - 1));
+        document.getElementById("type_me").classList.add("currentrow" + l / 10);
+        document.getElementById("row" + (l / 10 + 2)).classList.remove("doNotDisplay");
+        document.getElementById("row" + (l / 10 - 1)).classList.add("doNotDisplay");
     }
 }
 
